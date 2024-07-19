9d1df8ad143637259c6dbb011cc4cf52876534b0
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2024-07-19 16:23:22 +0200

    create tests for comment class

---
+++ src/test/java/com/example/forumapp/model/entities/CommentTest.java
@@ -0,0 +1,32 @@
+package com.example.forumapp.model.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+        assertTrue(true);
+    }
+
+    @Test
+    void getText() {
+    }
+
+    @Test
+    void builder() {
+    }
+}
