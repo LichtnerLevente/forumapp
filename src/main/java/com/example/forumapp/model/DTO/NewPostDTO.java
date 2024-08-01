145ed2e47e9839c8f1db10a34676a523729bc97e
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2024-08-01 21:54:46 +0200

    implement NewPostDTO

--- src/main/java/com/example/forumapp/model/DTO/NewPostDTO.java
+++ src/main/java/com/example/forumapp/model/DTO/NewPostDTO.java
@@ -1,4 +1,13 @@
 package com.example.forumapp.model.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class NewPostDTO {
+    private String title;
+    private String description;
+    private long authorID;
+
 }
